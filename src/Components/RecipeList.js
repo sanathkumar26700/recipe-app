4adc9ec4e79a771c3accdbb6d120af04232379c4
sanathkumar26700 <53991484+sanathkumar26700@users.noreply.github.com>
2021-09-27 16:55:45 +0530

    refactor code
    code not in use has been removed

--- src/Components/RecipeList.js
+++ src/Components/RecipeList.js
@@ -9,11 +9,12 @@ export default function RecipeList({recipes}) {
                 <Recipes 
                 key={recipe.id}
                 {...recipe}
-                />
+                /> 
             )}
             </div>
-            <div className = 'recipe-list__add-recipe--btn-container'></div>
+            <div className = 'recipe-list__add-recipe--btn-container'>
             <button className= 'btn btn--primary'>Add Recipe</button>
+            </div>
         </div>
     )
 }
