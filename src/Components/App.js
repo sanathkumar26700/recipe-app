4fb2da9b97d249a313c43da38e65fe5380b62b3d
sanathkumar26700 <53991484+sanathkumar26700@users.noreply.github.com>
2021-10-01 00:12:44 +0530

    feat Add storage
    using useEffect hook, local storage is being used to store the data of the interactions in the recipe apps

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,24 @@
-import React ,{useState} from 'react';
+import React ,{useState,useEffect} from 'react';
 import RecipeList from './RecipeList';
 import '../CSS/App.css';
 import {v4 as uuidv4} from 'uuid'
 
 export const RecipeContext = React.createContext()
+const LOCAL_STORAGE_KEY = 'recipeapp.recipes'
 
 function App() {
 
   const [recipes,setRecipes] = useState(sampleRecipes)
 
+  useEffect(() =>{
+    const recipeJson = localStorage.getItem(LOCAL_STORAGE_KEY)
+    if(recipeJson !== null) setRecipes(JSON.parse(recipeJson))
+  },[])
+
+  useEffect(() =>{
+    localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(recipes))
+  },[recipes])
+  
   const recipeContextValue = {
     handleRecipeAdd,
     handleRecipeDelete
