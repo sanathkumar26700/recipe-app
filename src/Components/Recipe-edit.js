6742ccd377116fc837535b739e807403d0f90700
sanathkumar26700 <53991484+sanathkumar26700@users.noreply.github.com>
2021-10-04 17:04:57 +0530

    Style  :  add styling to Recipe edit section

--- src/Components/Recipe-edit.js
+++ src/Components/Recipe-edit.js
@@ -1,31 +1,70 @@
 import React from 'react'
+import RecipeIngredientEdit from './Recipe-ingredient-edit'
 
 function RecipeEdit() {
     return (
         <div className="recipe-edit">
-            <div>
-                <button>&times;</button>
+            <div className= "recipe-edit__remove-button-container">
+                <button className="btn recipe-edit__remove-button">&times;</button>
             </div>
-            <div>
-                <label htmlFor="name">Name</label>
-                <input type="text" name="name" id="name"/>
-                <label htmlFor="cookTime">Cook Time</label>
-                <input type="text" name="cookTime" id="cookTime"/>
-                <label htmlFor="servings">Servings</label>
-                <input type="text" name="servings" id="servings"/>
-                <label htmlFor="instructions">Instructions</label>
-                <textarea name="instructions" id="instructions"></textarea>
+            <div className="recipe-edit__details-grid">
+                <label 
+                    htmlFor="name"
+                    className="recipe-edit__label">
+                    Name
+                </label>
+                <input 
+                    type="text" 
+                    name="name" 
+                    id="name"
+                    className="recipe-edit__input"
+                />
+                <label 
+                    htmlFor="cookTime"
+                    className="recipe-edit__label">
+                    Cook Time
+                </label>
+                <input 
+                    type="text" 
+                    name="cookTime" 
+                    id="cookTime"
+                    className="recipe-edit__input"
+                />                
+                <label 
+                    htmlFor="servings"
+                    className="recipe-edit__label">
+                    Servings
+                </label>
+                <input 
+                    type="text" 
+                    name="servings" 
+                    id="servings"
+                    className="recipe-edit__input"
+                />
+                <label 
+                    htmlFor="instructions"
+                    className="recipe-edit__label">
+                    Instructions
+                </label>
+                <textarea 
+                    name="instructions" 
+                    id="instructions"
+                    className= "recipe-edit__input">
+                </textarea>
             </div>
             <br/>
-            <label>Ingredients</label>
-            <div>
+            <label className="recipe-edit__label">
+            Ingredients
+            </label>
+            <div className="recipe-edit__ingredient-grid">
                 <div>Name</div>
                 <div>Amount</div>
                 <div></div>
-                {/* {} */}
+                <RecipeIngredientEdit/>
+                <RecipeIngredientEdit/>
             </div>
-            <div>
-                <button>Add ingredients</button>
+            <div className="recipe-edit__add-ingredient-btn-container">
+                <button className= "btn btn--primary">Add ingredients</button>
             </div>
         </div>
     )
