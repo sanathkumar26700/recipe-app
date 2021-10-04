22fff0f7fbdb3a1c44b7fbe436b38b6bada195b8
sanathkumar26700 <53991484+sanathkumar26700@users.noreply.github.com>
2021-10-04 17:00:20 +0530

    Create Recipe ingredient edit componet

---
+++ src/Components/Recipe-ingredient-edit.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function RecipeIngredientEdit() {
+    return (
+        <>
+         <input type="text"/>
+         <input type="text"/>
+         <button className= " btn btn--delete">&times;</button>
+        </>
+    )
+}
+
+export default RecipeIngredientEdit
